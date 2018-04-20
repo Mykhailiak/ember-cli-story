7802c5a0a7dcfde5edfe7550141d69288afed87c
Yura Myhaylyak <ghost86421@gmail.com>
2018-04-21 00:47:33 +0300

    Basis implemention of story cli

---
+++ buildStories.js
@@ -0,0 +1,1 @@
+console.log('Stories has been builded successful');
--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 /* jshint node: true */
 'use strict';
 
+const watch = require('node-watch');
+const path = require('path');
+const { exec } = require('child_process');
+
+const statuses = { CHANGE: 'change', ERROR: 'error' };
+const pathToComponent = path.join(__dirname, '/app/pods/components/');
+const watcher = watch(pathToComponent, { recursive: true, filter: /story\.js$/gi });
+
+watcher.on(statuses.CHANGE, async () => {
+  console.info('Updating stories...');
+
+  try {
+    await exec('node buildStories.js')
+
+    console.info('Stories has been updated successfully!');
+  } catch (e) {
+    console.error('Error: ', e);
+  }
+});
+
+watcher.on(statuses.ERROR, (e) => console.error('Error: ', e));
+
 module.exports = {
   name: 'ember-cli-story'
 };
