ad4b1f0a5f2abdb8b2706ff5b651f466d56b4a7f
Yura Myhaylyak <ghost86421@gmail.com>
2018-04-21 13:21:05 +0300

    Reimplementation

--- index.js
+++ index.js
@@ -5,24 +5,63 @@ const watch = require('node-watch');
 const path = require('path');
 const { exec } = require('child_process');
 
-const statuses = { CHANGE: 'change', ERROR: 'error' };
-const pathToComponent = path.join(__dirname, '/app/pods/components/');
-const watcher = watch(pathToComponent, { recursive: true, filter: /story\.js$/gi });
-
-watcher.on(statuses.CHANGE, async () => {
-  console.info('Updating stories...');
-
-  try {
-    await exec('node buildStories.js')
-
-    console.info('Stories has been updated successfully!');
-  } catch (e) {
-    console.error('Error: ', e);
-  }
-});
-
-watcher.on(statuses.ERROR, (e) => console.error('Error: ', e));
-
 module.exports = {
-  name: 'ember-cli-story'
+  name: 'ember-cli-story',
+  app: {
+    project: {
+      root: __dirname,
+    }
+  },
+  states: {
+    CHANGE: 'change',
+    ERROR: 'error',
+  },
+  paths: function() {
+    console.log(this.app.project.root);
+
+    return {
+      components: path.join(this.app.project.root, '/app/pods/components/'),
+    };
+  },
+  async changeHandler() {
+    console.info('========================================= stories =================');
+    console.info('Updating stories...');
+
+    try {
+      await exec('node buildStories.js')
+
+      console.info('Stories has been updated successfully!');
+    } catch (e) {
+      console.error('Error: ', e);
+    }
+
+    console.info('========================================= stories =================');
+  },
+  errorHandler(e) {
+    console.log('Error: ', e);
+  },
+  setupSubscribers() {
+    const { CHANGE, ERROR } = this.states;
+
+    this.watcher.on(CHANGE, (...args) => this.changeHandler(...args));
+    this.watcher.on(ERROR, (e) => this.errorHandler(e));
+  },
+  setup() {
+    this.watcher = watch(this.paths().components, {
+      recursive: true,
+      filter: /story\.js$/gi,
+      delay: 5000,
+    });
+
+    console.log('setup');
+
+    this.setupSubscribers();
+  },
+  included() {
+    // this._super.included(...arguments);
+
+    this.setup();
+  },
 };
+
+module.exports.included();
