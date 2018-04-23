096db236a8b1f89de4de83294e493eadd9ab9fbc
Yura Myhaylyak <ghost86421@gmail.com>
2018-04-23 21:00:29 +0300

    Add story blueprint

---
+++ blueprints/story/index.js
@@ -0,0 +1,34 @@
+/* jshint node:true */
+const fs = require('fs');
+const path = require('path');
+
+const isExist = p => fs.existsSync(p);
+
+module.exports = {
+  description: 'Create component story for combook',
+  fileMapTokens() {
+    return {
+      __componentName__(options) {
+        return options.dasherizedModuleName;
+      },
+      __name__() {
+        return 'story';
+      },
+    };
+  },
+  beforeInstall(options) {
+    const { entity: { name } } = options;
+    const pilotPath = path.join(__dirname, `../../app/pods/components/${name}`);
+
+    console.log('PATH', pilotPath);
+
+    if (!isExist(pilotPath)) {
+      throw new Error('Component doesn\'t exist yet.');
+    }
+  },
+  locals(options) {
+    return {
+      componentName: options.entity.name,
+    };
+  },
+};
