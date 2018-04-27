5bd930bdddf11dedefe6394884ce2bce7f7601b3
Yura Myhaylyak <ghost86421@gmail.com>
2018-04-28 00:26:14 +0300

    Style blueprint implementation

--- blueprints/style/index.js
+++ blueprints/style/index.js
@@ -1,15 +1,28 @@
-/*jshint node:true*/
+/* jshint node:true */
+
+const helpers = {
+  getType(type) {
+    if (!type) {
+      console.warn('Please, set type parameter. Example: ember g style <name> --type=components');
+
+      throw new Error('type parameter should not be blank');
+    }
+
+    return type.split('=')[1];
+  },
+};
+
 module.exports = {
-  description: ''
-
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
-
-  // afterInstall: function(options) {
-  //   // Perform extra work here.
-  // }
+  description: 'Create styles',
+  type: process.argv.find(k => k.includes('--type=')),
+  fileMapTokens() {
+    return {
+      __name__(options) {
+        return options.dasherizedModuleName;
+      },
+      __type__() {
+        return helpers.getType(this.type);
+      },
+    };
+  },
 };
